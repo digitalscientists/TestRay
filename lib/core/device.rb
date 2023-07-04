f5b6c5e53d865bc9209eed90c114f1d8044aacd3
Debbie Guzman <dguzman@neural.com.do>
2023-07-04 10:56:07 -0400

    fixing screenshots in two Actions

--- lib/core/device.rb
+++ lib/core/device.rb
@@ -764,7 +764,7 @@ class Device
   # Accepts:
   #   Strategy
   #   Id
-  def clear_field_by_backspace(action) 
+  def clear_field_by_backspace(action, main_case, main_case_id) 
     action = convert_value_pageobjects(action);
     el = nil
     el = wait_for(action) if (!action["Actions"] && action["Strategy"])
@@ -781,7 +781,7 @@ class Device
       end
     end
     if error && !action["NoRaise"]
-      path = take_error_screenshot()
+      path = take_error_screenshot(main_case, main_case_id)
       raise "#{@role}: #{error.message}\nError Screenshot: #{path}"
     end
   end
@@ -817,10 +817,25 @@ class Device
   #   Id
   def clear_field_js(action, main_case, main_case_id) 
     action = convert_value_pageobjects(action);
-    el = wait_for(action)
+    el = nil
+    el = wait_for(action, main_case, main_case_id) if (!action["Actions"] && action["Strategy"])
     
-    @driver.execute_script("arguments[0].value='';", el)
-    sleep(1.0)
+    start = Time.now
+    error = nil
+    while (Time.now - start) < @timeout
+      begin
+        @driver.execute_script("arguments[0].value='';", el)
+        sleep(1.0)
+        return
+      rescue => e
+        error = e
+      end
+    end
+    if error && !action["NoRaise"]
+      path = take_error_screenshot(main_case, main_case_id)
+
+      raise "#{@role}: #{error.message}\nError Screenshot: #{path}"
+    end
   end
 
   # Accepts:
